517f430f4a06ccaadeb76beaeac7f629144200ff
raze <155824568+razemsb@users.noreply.github.com>
2024-10-28 09:27:24 +0300

    Update reg.js

--- script/reg.js
+++ script/reg.js
@@ -1,1 +1,21 @@
+const message = document.getElementById("uploadMessage");
+if (message.innerHTML) {
+    message.style.display = "block";
+    setTimeout(() => {
+        message.classList.add("fade-out");
+        setTimeout(() => {
+            message.style.display = "none";
+        }, 1000); // время анимации изчезновения
+    }, 3000);  // время существования message
+}
+document.getElementById("switchToLogin").addEventListener("click", function(e) {
+e.preventDefault();
+document.getElementById("form2").classList.add("hidden");
+document.getElementById("form1").classList.remove("hidden");
+});
 
+document.getElementById("switchToRegister").addEventListener("click", function(e) {
+e.preventDefault();
+document.getElementById("form2").classList.remove("hidden");
+document.getElementById("form1").classList.add("hidden");
+});
