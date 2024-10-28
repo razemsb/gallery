40c123f9be4ace3ea173348a770389c536e0793c
raze <155824568+razemsb@users.noreply.github.com>
2024-10-28 09:26:59 +0300

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,1 +1,30 @@
+const message = document.getElementById('uploadMessage');
+if (message.innerHTML) {
+    message.style.display = 'block';
+    setTimeout(() => {
+        message.classList.add('fade-out');
+        setTimeout(() => {
+            message.style.display = 'none';
+        }, 1000); // время анимации изчезновения
+    }, 3000);  // время существования message
+}
 
+function openModal(filename, user, date) {
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById('modalImg');
+const modalUser = document.getElementById('modalUser');
+const modalDate = document.getElementById('modalDate');
+const modalCaption = document.getElementById('modalCaption');
+
+modal.style.display = "flex"; 
+modalImg.src = "files/" + filename;
+modalUser.innerHTML = user;
+modalDate.innerHTML = date;
+modalCaption.innerHTML = filename;
+}
+
+// Функция для закрытия модального окна
+function closeModal() {
+const modal = document.getElementById('myModal');
+modal.style.display = "none"; // Скрываем модальное окно
+}
